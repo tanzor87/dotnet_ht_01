bb66f738ab42a828a3936610121bd2eeaf9626ac
Andrey <tanzor87@mail.ru>
2023-10-18 00:57:08 +0700

    home task 1

---
+++ task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("ВВедите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else if (number2> number1 && number2 > number3)
+{
+    Console.WriteLine($"max = {number2}");
+}
+else if (number3> number1 && number3 > number2)
+{
+    Console.WriteLine($"max = {number3}");
+}
+else
+{
+    Console.WriteLine("Введены не корректные значения");
+}
---
+++ task_6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("ВВедите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else if (number % 2 != 0)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Введено не корректное значение");
+}
